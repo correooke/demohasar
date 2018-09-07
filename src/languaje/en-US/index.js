504d1b3aff11eabc3384a1ac8b0a27d1397fc1d3
HSNBDES33\eocariz <eocariz@hasar.com>
2018-09-07 09:57:22 -0300

    Multilenguaje mas flow

---
+++ src/languaje/en-US/index.js
@@ -0,0 +1,12 @@
+export default { 
+    'GENERIC.ADD': 'Add',
+    'GENERIC.EDIT': 'Edit',
+    'GENERIC.SEARCH': 'Search',
+    'GENERIC.TITLE': 'Title',
+    'APP.TITLE': 'Studing App',
+    'GENERIC.WELCOME': `Hello {name}, you have {unreadCount, plural,
+        =0 {no message}
+        one { {unreadCount, number} message}
+        other { {unreadCount, number} messages}
+      }`,
+}
